5ae1e39b20ab328a5799c2edc790a0ac0e99c2c9
BeatleJuice <46233616+BeatleJuicePack@users.noreply.github.com>
2019-08-23 16:27:42 -0700

    Create autoSearch.py

---
+++ autoSearch.py/autoSearch.py
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################################################################################
+##   AutoSearch - Python 3.6                                                  ##
+##   August 2019 BeatleJuice - BeatleJuicePack                                ##
+##   https://beatlejuicepack.com/                                             ##
+##   https://github.com/BeatleJuicePack                                       ##
+################################################################################
+##   This program is free software: you can redistribute it and/or modify     ##
+##   it under the terms of the GNU General Public License as published by     ##
+##   the Free Software Foundation, either version 3 of the License, or        ##
+##   (at your option) any later version.                                      ##
+##                                                                            ##
+##   This program is distributed in the hope that it will be useful,          ##
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of           ##
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            ##
+##   GNU General Public License for more details.                             ##
+##                                                                            ##
+##   You should have received a copy of the GNU General Public License        ##
+##   along with this program.  If not, see <https://www.gnu.org/licenses/>.   ##
+################################################################################
+
+try: 
+    from googlesearch import search 
+except ImportError:  
+    print("No module named 'google' found") 
+  
+# to search 
+query = input("Enter Your Search: ")
+  
+for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
+    print(j) 
